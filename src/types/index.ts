c220b0a9c50a37e46ba3c58fd4e9d9b958d0b595
suchenxiaoyu <suchenxiaoyu@gmail.com>
2021-01-07 17:35:38 +0800

    feat(simple): simple demo

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
+
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'post'
+  | 'POST'
+  | 'head'
+  | 'HEAD'
+  | 'put'
+  | 'PUT'
+  | 'delete'
+  | 'DELETE'
+  | 'options'
+  | 'OPTIONS'
+  | 'patch'
+  | 'PATCH'
---
+++ src/xhr.ts
@@ -0,0 +1,9 @@
+import { AxiosRequestConfig } from './types'
+
+export default function xhr(config: AxiosRequestConfig) {
+  const { url, data = null, method = 'get' } = config
+
+  const request = new XMLHttpRequest()
+  request.open(method.toUpperCase(), url, true)
+  request.send(data)
+}
