e835f80db0651169fbfe90a506a4e60e061607a2
suchenxiaoyu <suchenxiaoyu@gmail.com>
2021-01-26 19:22:45 +0800

    'interceptor'

--- src/types/index.ts
+++ src/types/index.ts
@@ -13,7 +13,7 @@ export interface RejectedFn {
 export interface Axios {
   interceptors: {
     request: AxiosInterceptorManager<AxiosRequestConfig>
-    response: AxiosInterceptorManager<AxiosPromise>
+    response: AxiosInterceptorManager<AxiosResponse>
   }
 
   request<T = any>(config: AxiosRequestConfig): AxiosPromise<T>
